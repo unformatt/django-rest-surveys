3bc034c5f8fdcce2064ea4cc2befb122ca916504
danxshap <registration@dshap.com>
2016-10-28 00:35:28 -0400

    change settings format to be consistent with swapper

--- rest_surveys/views.py
+++ rest_surveys/views.py
@@ -15,26 +15,29 @@ SurveyResponse = swapper.load_model('rest_surveys', 'SurveyResponse')
 class SurveyResponseViewSet(mixins.RetrieveModelMixin,
                             mixins.CreateModelMixin,
                             mixins.UpdateModelMixin,
-                            mixins.ListModelMixin,
                             viewsets.GenericViewSet):
     queryset = SurveyResponse.objects.all()
     serializer_class = to_class(
             getattr(settings, 'REST_SURVEYS_SURVEYRESPONSE_SERIALIZER',
                     'rest_surveys.serializers.SurveyResponseSerializer'))
-    authentication_classes = settings.REST_SURVEYS.get(
-           'SURVEY_RESPONSE_AUTHENTICATION_CLASSES', (SessionAuthentication,))
-    permission_classes = settings.REST_SURVEYS.get(
-           'SURVEY_RESPONSE_PERMISSION_CLASSES', (IsAuthenticated,))
+    authentication_classes = getattr(
+        settings, 'REST_SURVEYS_SURVEYRESPONSE_AUTHENTICATION_CLASSES',
+        (SessionAuthentication,))
+    permission_classes = getattr(
+        settings, 'REST_SURVEYS_SURVEYRESPONSE_PERMISSION_CLASSES',
+        (IsAuthenticated,))
     filter_backends = (filters.DjangoFilterBackend,)
-    filter_fields = settings.REST_SURVEYS.get(
-           'SURVEY_RESPONSE_FILTER_FIELDS',
-           get_field_names(SurveyResponse))
+    filter_fields = getattr(settings,
+                            'REST_SURVEYS_SURVEYRESPONSE_FILTER_FIELDS',
+                            get_field_names(SurveyResponse))
 
 
 class SurveyViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Survey.objects.all()
     serializer_class = SurveySerializer
-    authentication_classes = settings.REST_SURVEYS.get(
-           'SURVEY_AUTHENTICATION_CLASSES', (SessionAuthentication,))
-    permission_classes = settings.REST_SURVEYS.get(
-           'SURVEY_PERMISSION_CLASSES', (IsAuthenticated,))
+    authentication_classes = getattr(
+        settings, 'REST_SURVEYS_SURVEY_AUTHENTICATION_CLASSES',
+        (SessionAuthentication,))
+    permission_classes = getattr(
+        settings, 'REST_SURVEYS_SURVEY_PERMISSION_CLASSES',
+        (IsAuthenticated,))
