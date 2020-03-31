49e198bd890218fce64e2b728d1b37fcac0628c7
Edgar M. Salazar <edgarmsalazar@gmail.com>
2020-03-31 16:25:24 -0400

    making upgrades to fix filters and older packages

--- rest_surveys/views.py
+++ rest_surveys/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.conf import settings
-from rest_framework import filters, mixins, viewsets
+from rest_framework import mixins, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_surveys.serializers import SurveySerializer
 from rest_surveys.utils import get_field_names, to_class
 
@@ -32,7 +33,7 @@ class SurveyResponseViewSet(mixins.RetrieveModelMixin,
         'REST_SURVEYS_SURVEYRESPONSE_PERMISSION_CLASSES',
         ['rest_framework.permissions.IsAuthenticated']
     )]
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filter_fields = getattr(settings,
                             'REST_SURVEYS_SURVEYRESPONSE_FILTER_FIELDS',
                             get_field_names(SurveyResponse))
